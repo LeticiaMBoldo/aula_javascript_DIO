80376a46b49b55305c18b2900f92afe24f791ec9
LeticiaBoldo <leticiamboldo14@gmail.com>
2021-04-14 21:00:39 -0300

    Terceira aula do curso, condicionais, laços de repetição e Date

---
+++ Condicionais&laçosRepeticao&Date/JS/main.js
@@ -0,0 +1,43 @@
+//var idade = prompt("qual é a sua idade?"); //prompt monstra uma caixa de para que responda algo na tela.
+
+// var count = 0;
+
+// while (count < 5)
+// {
+//     console.log(count);
+//     count++;
+// }
+
+// for(x = 0; x < 5; x++)
+// {
+//     // alert(x);
+//     console.log(x);
+// }
+
+// console.log(count);
+
+/*
+if(idade > 18)
+{
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+*/
+
+/* ---------------------------------------- */
+/**comando data */
+//data atual
+var d = new Date();
+// alert(d.getDate()); //dia
+// alert(d.getDay());//dia e mes
+// alert(d.getMinutes()); //pega as horas
+// alert(d.getMonth() + 1) //pegar o mes certo
+
+
+//alert("digirak" + formacoes);
+
+trajeto = ["mae", "floresta", "avo"];
+
+console.log(trajeto.toString());
